be8d26d9107e6e182dbb986d8c9d123942a0adca
Andy Cheng <ccjeng@gmail.com>
2022-04-27 18:52:10 +0800

    timeout

--- core/lol.py
+++ core/lol.py
@@ -7,7 +7,12 @@ from bs4 import BeautifulSoup
 import time
 import datetime
 
+
 count=1200
+
+target="https://ibackup1230.pixnet.net/blog"
+timeout=100 
+stay=5
 
 ## os.system("apt install toilet")
 color1=["\033[1;31;40m","\033[1;32;40m","\033[1;33;40m","\033[1;34;40m","\033[1;35;40m","\033[1;36;40m"]
@@ -15,7 +20,7 @@ def color():
  return str(random.choice(color1))
 def banner():
  os.system("clear")
-## os.system("toilet -fmono12 -F gay Bot")
+ #os.system("toilet -fmono12 -F gay Bot")
  print("    \033[1;36;40m Code made by: \033[1;32;40m tuhin1729")
  print("    \033[1;36;40m Instagram id: \033[1;32;40m www.instagram.com/tuhin1729")
  print("    \033[1;36;40m Github      : \033[1;32;40m www.github.com/tuhin1729")
@@ -48,10 +53,12 @@ def req(proxy_url,target_url,timeo,stay_time):
    j=j+8
   except:
    break
+ counter=0  
  for index in range (0,(len(iplist)-1)): 
   try:   
    PROXY=iplist[index]+":"+portlist[index]
-   print("\033[1;32;40m"+str(datetime.datetime.now())+" Trying From "+str(PROXY))
+   counter=counter+1
+   print("\033[1;32;40m"+str(datetime.datetime.now())+" Trying From "+str(PROXY)+" - "+str(counter))
    webdriver.DesiredCapabilities.FIREFOX['proxy']={"httpProxy":PROXY,"sslProxy":PROXY,"proxyType":"MANUAL"}
    driver=webdriver.Firefox()
    driver.set_page_load_timeout(timeo)
@@ -60,17 +67,14 @@ def req(proxy_url,target_url,timeo,stay_time):
    driver.close()
   except Exception as e: 
    driver.close()
-banner()
-target="https://ibackup1230.pixnet.net/blog" # input("\033[1;33;40mEnter The Target URL(Ex: https://www.google.com) :")
-if("tuhin1729" in target):
- print("You can't perform it in my Website")
- sys.exit()
-timeout=100 #int(input("\033[1;33;40mEnter The Time Out(In Seconds)(Recommended 100) :"))
-stay=10 #int(input("\033[1;33;40mEnter The Stay Time(In Seconds) :"))
+#banner()
+#target=input("\033[1;33;40mEnter The Target URL(Ex: https://www.google.com) :")
+timeout=int(input("\033[1;33;40mEnter The Time Out(In Seconds)(Recommended 100) :"))
+#stay=int(input("\033[1;33;40mEnter The Stay Time(In Seconds) :"))
 urllist=["https://www.sslproxies.org","https://us-proxy.org","https://free-proxy-list.net/uk-proxy.html","https://free-proxy-list.net/anonymous-proxy.html","https://free-proxy-list.net","https://www.socks-proxy.net"]
 
 for purl in urllist:
- banner()
+ #banner()
  req(purl,target,timeout,stay)
  print("\033[1;32;40mSleeping For 10 Seconds")
  time.sleep(10)
