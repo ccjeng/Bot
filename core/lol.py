ed940bd2468a2a53f0c35e6c0268c9e49592d5fe
Andy Cheng <ccjeng@gmail.com>
2022-04-28 22:19:38 +0800

    add country/code

--- core/lol.py
+++ core/lol.py
@@ -13,23 +13,6 @@ count=1200
 target="https://ibackup1230.pixnet.net/blog"
 timeout=100 
 stay=5
-
-## os.system("apt install toilet")
-color1=["\033[1;31;40m","\033[1;32;40m","\033[1;33;40m","\033[1;34;40m","\033[1;35;40m","\033[1;36;40m"]
-def color():
- return str(random.choice(color1))
-def banner():
- os.system("clear")
- #os.system("toilet -fmono12 -F gay Bot")
- print("    \033[1;36;40m Code made by: \033[1;32;40m tuhin1729")
- print("    \033[1;36;40m Instagram id: \033[1;32;40m www.instagram.com/tuhin1729")
- print("    \033[1;36;40m Github      : \033[1;32;40m www.github.com/tuhin1729")
- print("    \033[1;36;40m YouTube     : \033[1;32;40m https://bit.ly/TuhinTheHacker")
- print("    \033[1;36;40m Dedicated to: \033[1;34;40m Diya Saha")
- print("        \033[1;31;40mSome Proxies May Be Dead. :(")
- print("        \033[1;31;40mRemember:The Website In Which You Are Testing May Identify This Bot.")
- print("\n\n") 
-
 def req(proxy_url,target_url,timeo,stay_time):
  r=requests.get(proxy_url)
  soup=BeautifulSoup(r.content,'html.parser')
@@ -37,6 +20,8 @@ def req(proxy_url,target_url,timeo,stay_time):
  list1 = list.findAll('td')
  iplist=[]
  portlist=[]
+ codelist=[]
+ countrylist=[]
  j=0
  while(j<(8*count)):
   try:
@@ -53,12 +38,29 @@ def req(proxy_url,target_url,timeo,stay_time):
    j=j+8
   except:
    break
- counter=0  
+ j=2
+ while(j<(8*count)):
+  try:
+   a=str(list1[j].contents[0])
+   codelist.append(a)
+   j=j+8
+  except:
+   break
+ j=3
+ while(j<(8*count)):
+  try:
+   a=str(list1[j].contents[0])
+   countrylist.append(a)
+   j=j+8
+  except:
+   break
+ counter=0   
  for index in range (0,(len(iplist)-1)): 
   try:   
    PROXY=iplist[index]+":"+portlist[index]
+   #if codelist[index]=="TH" or codelist[index]=="SG" or codelist[index]=="HK" or codelist[index]=="CN" or codelist[index]=="IN" or codelist[index]=="IR" or codelist[index]=="ID" or codelist[index]=="JP":
    counter=counter+1
-   print("\033[1;32;40m"+str(datetime.datetime.now())+" Trying From "+str(PROXY)+" - "+str(counter))
+   print("\033[1;32;40m"+"#"+str(counter)+" "+str(datetime.datetime.now())+" Trying From "+str(PROXY)+" ("+countrylist[index]+")")
    webdriver.DesiredCapabilities.FIREFOX['proxy']={"httpProxy":PROXY,"sslProxy":PROXY,"proxyType":"MANUAL"}
    driver=webdriver.Firefox()
    driver.set_page_load_timeout(timeo)
@@ -66,16 +68,16 @@ def req(proxy_url,target_url,timeo,stay_time):
    time.sleep(stay_time)
    driver.close()
   except Exception as e: 
-   driver.close()
-#banner()
+    driver.close()
+
 #target=input("\033[1;33;40mEnter The Target URL(Ex: https://www.google.com) :")
 timeout=int(input("\033[1;33;40mEnter The Time Out(In Seconds)(Recommended 100) :"))
 #stay=int(input("\033[1;33;40mEnter The Stay Time(In Seconds) :"))
-urllist=["https://www.sslproxies.org","https://us-proxy.org","https://free-proxy-list.net/uk-proxy.html","https://free-proxy-list.net/anonymous-proxy.html","https://free-proxy-list.net","https://www.socks-proxy.net"]
+urllist=["https://www.sslproxies.org","https://free-proxy-list.net/anonymous-proxy.html","https://free-proxy-list.net","https://www.socks-proxy.net","https://us-proxy.org","https://free-proxy-list.net/uk-proxy.html"]
 
+print("\n\033[1;32;40mTarget Site: "+target+", Timeout: "+str(timeout)+"\n") 
 for purl in urllist:
- #banner()
  req(purl,target,timeout,stay)
- print("\033[1;32;40mSleeping For 10 Seconds")
- time.sleep(10)
+ print("\033[1;32;40mSleeping For 3 Seconds")
+ time.sleep(3)
  
